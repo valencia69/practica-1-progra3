75e3080c20b9fd17e63da6d9de156fc948de4ddf
Nestor Valencia <nestor valencia>
2021-07-06 14:11:06 -0400

    clase 7

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -18,6 +18,7 @@ class DescriptionPlace extends StatelessWidget{
         child: Text(
           textoTitulo,
           style: TextStyle(
+            fontFamily: "Lato",
             fontSize: 38,
             fontWeight: FontWeight.bold
           ),
@@ -69,6 +70,7 @@ class DescriptionPlace extends StatelessWidget{
       child: Text(
           textoDescripcion,
         style: TextStyle(
+          fontFamily: "Lato",
           color: Colors.black45
       ),
 
